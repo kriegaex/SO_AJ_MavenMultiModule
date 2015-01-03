32ee5fc6bc8bcb4e358098865b252845b2bb5192
Alexander Kriegisch <Alexander@Kriegisch.name>
2015-01-03 07:34:58 +0100

    Make aspect mix profile-dependent in Maven
    There are now two Maven profiles 'javabean' and 'json' which are active
    by default, i.e. the corresponding aspects are woven into the core code
    automatically. You can remove each aspect type from the mix by
    deactivating profiles via '-P !javabean' from the command line.
    In IntelliJ IDEA you can just use the checkboxes in the
    "Maven Projects" view.
    
    BTW, the sample application in module 'client-jar' was changed to use
    reflection and exception handling for ITD method invocation in order to
    make it compile and function dynamically.

--- client-jar/src/main/java/de/scrum_master/app/Application.java
+++ client-jar/src/main/java/de/scrum_master/app/Application.java
@@ -1,18 +1,30 @@
 package de.scrum_master.app;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Sample application using 'SampleObject' class from module 'data-objects' as well as
  * ITD aspects from modules 'aspect-javabean' and 'aspect-json'
  */
 public class Application {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
 		SampleObject sampleObject = new SampleObject("John Doe", 33);
 		System.out.println(sampleObject);
+
 		System.out.println("\nJava Bean properties:");
-		System.out.println("  " + sampleObject.getName());
-		System.out.println("  " + sampleObject.getAge());
+		try {
+			System.out.println("  " + SampleObject.class.getMethod("getName").invoke(sampleObject));
+			System.out.println("  " + SampleObject.class.getMethod("getAge").invoke(sampleObject));
+		} catch (NoSuchMethodException e) {
+			System.out.println("  unavailable (inactive aspect)");
+		}
+
 		System.out.println("\nJSON properties:");
-		System.out.println("  " + sampleObject.getNameValue());
-		System.out.println("  " + sampleObject.getAgeValue());
+		try {
+			System.out.println("  " + SampleObject.class.getMethod("getNameValue").invoke(sampleObject));
+			System.out.println("  " + SampleObject.class.getMethod("getAgeValue").invoke(sampleObject));
+		} catch (NoSuchMethodException e) {
+			System.out.println("  unavailable (inactive aspect)");
+		}
 	}
 }
